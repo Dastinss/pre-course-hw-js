2aa5ce5172d073d5f69017e0f7cb309a75653247
Dastinss <sergeyfdaragan@gmail.com>
2023-12-08 11:32:57 +0200

    feat(#2): exercise #2 redone due to remark

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,8 +1,7 @@
-const randomNumber = () => {
-	const number1 = Math.floor( Math.random() * 100 );
+const randomNumber = (number1 = Math.floor( Math.random() * 100 )) => {
 	return number1 % 2
-		? console.log( number1 + ':Это нечетное число' )
-		: console.log( number1 + ':Это четное число' )
+		? number1 + ':Это нечетное число'
+		: number1 + ':Это четное число';
 }
 
-randomNumber();
+console.log(randomNumber())
