865e0ee472d65069e2f8270569ead4bb3d3063af
Dastinss <sergeyfdaragan@gmail.com>
2023-12-22 02:14:18 +0200

    feat(#2 lesson-3): exercise #2 redone

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -3,7 +3,7 @@
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
 
 function findOrange() {
-	return words.indexOf("апельсин") ? "Ура! нашел" : "Придется поискать в другом магазине…"
+	return words.indexOf("апельсин") !== -1 ? "Ура! нашел" : "Придется поискать в другом магазине…"
 }
 
 console.log( findOrange() )
