9445d1da607e7148db9f048a957dad34aeb42b5c
Dastinss <sergeyfdaragan@gmail.com>
2023-12-22 14:02:25 +0200

    feat(#3 lesson-3): exercise #3 redone

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -2,10 +2,10 @@
 
 const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
 
-function restFruits(  ) {
-	// let arr=[];
-	// arr.push(fruits.slice(0,1).join(' '), fruits.slice(fruits.length-2))
-	return fruits.slice(0,1).concat(fruits.slice(fruits.length-2))
-}
+// function restFruits(  ) {
+// 	// let arr=[];
+// 	// arr.push(fruits.slice(0,1).join(' '), fruits.slice(fruits.length-2))
+// 	return fruits.slice(0,1).concat(fruits.slice(fruits.length-2))
+// }
 
-console.log(restFruits())
+console.log(fruits.slice(0,1).concat(fruits.slice(fruits.length-2)))
