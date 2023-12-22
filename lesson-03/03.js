f3f0d8029096872f88f8ce8088128e2d7461cc6e
Dastinss <sergeyfdaragan@gmail.com>
2023-12-22 15:39:07 +0200

    feat(#3 lesson-3): exercise #3 redone-change condition

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -2,10 +2,4 @@
 
 const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
 
-// function restFruits(  ) {
-// 	// let arr=[];
-// 	// arr.push(fruits.slice(0,1).join(' '), fruits.slice(fruits.length-2))
-// 	return fruits.slice(0,1).concat(fruits.slice(fruits.length-2))
-// }
-
-console.log(fruits.slice(0,1).concat(fruits.slice(fruits.length-2)))
+console.log(fruits.slice(1,fruits.length-2))
